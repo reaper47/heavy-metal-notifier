47a2db55cf9d7f7b893145cd98caf8ac2dd3bb23
reaper47 <macpoule@gmail.com>
2023-04-06 13:21:23 +0200

    improved check releases job

--- internal/jobs/releases.go
+++ internal/jobs/releases.go
@@ -13,24 +13,25 @@ import (
 )
 
 func ScheduleCheckReleases(service services.Service) {
-	if _, err := app.Scheduler.Every(1).Day().Do(func() {
-		users, err := service.Users()
-		if err != nil {
-			email.Send(app.Config.Email.From, constants.EmailErrorAdmin, templates.DataError{
-				Text: fmt.Sprintf("releases.go: error getting users: %q", err),
-			})
-			return
-		}
-
+	if _, err := app.Scheduler.Every(1).Day().At("01:00").Do(func() {
 		now := time.Now()
 		releases := app.Calendar.ReleasesOnDate(now.Month(), now.Day())
-		for _, user := range users {
-			email.Send(user.Email, constants.EmailReleases, templates.EmailData{
-				EmailBase64: base64.StdEncoding.EncodeToString([]byte(user.Email)),
-				Name:        strings.Split(user.Email, "@")[0],
-				Releases:    releases,
-				URL:         app.Config.URL,
-			})
+		if len(releases) > 0 {
+			users, err := service.Users()
+			if err != nil {
+				email.Send(app.Config.Email.From, constants.EmailErrorAdmin, templates.DataError{
+					Text: fmt.Sprintf("releases.go: error getting users: %q", err),
+				})
+				return
+			}
+			for _, user := range users {
+				email.Send(user.Email, constants.EmailReleases, templates.EmailData{
+					EmailBase64: base64.StdEncoding.EncodeToString([]byte(user.Email)),
+					Name:        strings.Split(user.Email, "@")[0],
+					Releases:    releases,
+					URL:         app.Config.URL,
+				})
+			}
 		}
 	}); err != nil {
 		email.Send(app.Config.Email.From, constants.EmailErrorAdmin, templates.DataError{
