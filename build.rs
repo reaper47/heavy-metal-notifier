bc159aa68ca0db665f338f1f20ac9924f80f6007
reaper47 <macpoule@gmail.com>
2025-01-02 22:40:05 -0500

    Try to install nodejs

--- build.rs
+++ build.rs
@@ -1,14 +1,6 @@
 use std::process::Command;
 
 fn main() {
-    let status = Command::new("npm").args(["-v"]).status()
-        .expect("NPOM not installed");
-
-    if !status.success() {
-        eprintln!("npm failed with status: {}", status);
-        std::process::exit(1);
-    }
-
     let status = Command::new("npm")
         .args(["--prefix", "./web/app", "install", "./web/app"])
         .status()
